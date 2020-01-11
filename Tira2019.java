9d913cde37e4383a8fdb18d166acb7c318e5bfa2
Aarre Leinonen <aarre.leinonen@tuni.fi>
2020-01-11 15:19:03 +0200

    eka commit, projektin luonti

---
+++ Tira2019.java
@@ -0,0 +1,45 @@
+/*
+*TiRa 2019 kurssin harjoitustyö
+*Aarre Leinonen, 428786
+*
+*
+*
+ */
+
+ import java.util.LinkedList;
+ import java.util.Scanner;
+
+public class Tira2019 {
+
+    //Julkinen scanner syötteen lukuun.
+    Scanner input = new Scanner(system.in);
+
+    //luokka hajautustaulun ALKIOT
+    public class Node {
+
+        int key;
+        int value;
+        Node next;
+        Node prev;
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    //Luokka HAJAUTUSTAULU.
+    public class HashTable {
+
+        
+    }
+
+
+    public static void main(String[] args) {
+        Scanner file1 = new Scanner(setA.txt);
+        Scanner file2 = new Scanner(setB.txt);
+
+
+
+    }
+}
